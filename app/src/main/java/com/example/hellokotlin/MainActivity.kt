51fd4b4abe653f1176cd16b25a5f809457a13c46
pablorjd1 <pablo.jimenez2221@gmail.com>
2023-08-23 10:39:34 -0400

    optimizar importaciones

--- app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.hellokotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import java.util.Locale
 
 class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
